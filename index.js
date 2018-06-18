72aad8cc6542160e8847bcaad61a03715a6d3435
Christine Xiang <yutingcxiang@gmail.com>
2018-06-18 01:10:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function removeFromCart(item) {
      cart.splice(i, 1);
      return cart;
   } else {
-    return "That item is not in your cart."
+    return "That item is not in your cart.";
     }
   }
 }
