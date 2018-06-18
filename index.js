a3e26928df84888588b04398fd0d20571ac126df
Christine Xiang <yutingcxiang@gmail.com>
2018-06-18 01:18:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,11 +46,11 @@ function removeFromCart(item) {
     if (cart[i]["itemName"] === item) {
      cart.splice(i, 1);
      return cart;
-  } else {
-    return "That item is not in your cart.";
     }
   }
+  return "That item is not in your cart.";
 }
+
 
 function placeOrder(cardNumber) {
   // write your code here
