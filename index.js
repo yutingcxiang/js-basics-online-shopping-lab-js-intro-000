060cf3ee041a8d1c586a427fd40af08399812685
Christine Xiang <yutingcxiang@gmail.com>
2018-06-18 01:21:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
-  if (cardNumber < 1) {
+  if (!cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
     cart = [];
